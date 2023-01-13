64a20de5189121a6ddf06624f4a6d09cfc193bff
Ray-linux <ranirava52@gmail.com>
2023-01-13 10:20:25 +0530

    adding local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/form/Form";
 import Header from "./components/Header";
 import Students from "./components/students/Students";
 
 function App() {
-const data = [
+  let initStudens;
+  if(localStorage.getItem("students") === null){
+    initStudens = [];
+  }
+  else{
+    initStudens = JSON.parse( localStorage.getItem("students"));
+  }
+
+  const AddStudent = (name, email, website, image, sex) => {
+    let sno;
+    if(students.length===0){
+      sno = 0;
+    }
+    else {
+      sno = students[students.length-1].sno + 1;
+    }
+    const newStudents = {
+      sno: sno,
+      name: name,
+      email: email,
+      website : website,
+      image: image,
+      sex: sex,
+    }
+    setStudents([...students, newStudents]);
+    console.log(newStudents);
+  }
+
+  const [students, setStudents] = useState(initStudens);
+  useEffect(() => {
+    localStorage.setItem("students", JSON.stringify(students));
+  }, [students])
+
+
+  console.log(students)
+
+  const data = [
     {
       id: 1,
       email: "george.bluth@reqres.in",
       first_name: "George",
       last_name: "Bluth",
-      avatar: "https://reqres.in/img/faces/1-image.jpg",
+      avatar: "https://reqres.in/img/faces/8-image.jpg",
     },
   ];
   return (
     <>
       <Header title={"student enrolment form"} />
       <div className="main_page">
-      <Form/>
+      <Form addStudents = {AddStudent}/>
       <Students data = {data}/>
       </div>
     </>
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,48 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './form.css'
 
-export default function Form() {
+export default function Form(props) {
+
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [website, setWebsite] = useState("");
+const [image, setImage] = useState("");
+const [sex, setSex] = useState("");
+const  [reactjs, setReactjs] = useState("");
+const  [nodejs, setNodejs] = useState("")
+const  [angularjs, setAngularjs] = useState("")
+const  [nextjs, setNextjs] = useState("")
+
+const submit = (e) => {
+  e.preventDefault();
+  if(!name){
+    alert("Name can't be empty")
+  }
+  else{
+  props.addStudents(name, email, website, image, sex);
+    setName("")
+    setEmail("")
+    setWebsite("")
+    setImage("")
+    setSex("")
+  }
+}
+
+// console.log(sex)
+
   return (
     <>
-        <form className='form'>
+        <form className='form' onSubmit={submit}>
             <div className="name">
             <label htmlFor="name">Name</label>
-            <input type="text"  id="name" name = "name" required/>
+            <input type="text"  id="name" name = "name" value={name} onChange={(e) => {setName(e.target.value)}}/>
             </div>
             <div className="email">
             <label htmlFor="email">Email</label>
-            <input type="email"  id="email" name = "email" required/>
+            <input type="email"  id="email" name = "email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
             </div>
             <div className="web">
             <label htmlFor="website">Website</label>
-            <input type="text"  id="website" name = "website"/>
+            <input type="text"  id="website" name = "website" value={website} onChange={(e) => {setWebsite(e.target.value)}}/>
             </div>
             <div className="image">
             <label htmlFor="image">Image</label>
-            <input type="text"  id="image" name = "image" required/>
+            <input type="text"  id="image" name = "image" value={image} onChange={(e) => {setImage(e.target.value)}}/>
             </div>
             <div className="gender">
-            <label htmlFor="gender">Gender</label>
-            <input type="radio" id="gender" name = "gender" required/>
+            <label>Gender</label>
+            <input type="radio" id="gender" name = "gender"  value="male" onChange={(e) => {setSex(e.target.value)}}/>
             <label htmlFor="gender">Male</label>
-            <input type="radio" id="gender" name = "gender" required/>
+            <input type="radio" id="gender" name = "gender"  value="female" onChange={(e) => {setSex(e.target.value)}}/>
             <label htmlFor="gender">Female</label>
-            <input type="radio" id="gender" name = "gender" value="gender" required/>
+            <input type="radio" id="gender" name = "gender"  value="others" onChange={(e) => {setSex(e.target.value)}}/>
             <label htmlFor="gender">Others</label>
             </div>
             <div className="check_values">
-            <lable>Skills</lable>
-            <input type="checkbox" name="skill1" id="skill1" value = "java"/>
+            Skills
+            <input type="checkbox" name="reactjs" id="skill1" value="reactjs"onChange={(e) => {setReactjs(e.target.value)}}/>
             <label htmlFor="skill1">React js</label>
-            <input type="checkbox" name="skill1" id="skill2" value = "java"/>
-            <label htmlFor="skill2">Angular js</label>
-            <input type="checkbox" name="skill1" id="skill3" value = "java"/>
-            <label htmlFor="skill3">Next js</label>
-            <input type="checkbox" name="skill1" id="skill4" value = "java"/>
-            <label htmlFor="skill4">Node js</label>
+            <input type="checkbox" name="nodejs" id="skill2" value="nodejs" onChange={(e) => {setNodejs(e.target.value)}}/>
+            <label htmlFor="skill2">Node js</label>
+            <input type="checkbox" name="angularjs" id="skill3" value="angularjs" onChange={(e) => {setAngularjs(e.target.value)}}/>
+            <label htmlFor="skill3">Angular js</label>
+            <input type="checkbox" name="nextjs" id="skill4" value="nextjs" onChange={(e) => {setNextjs(e.target.value)}}/>
+            <label htmlFor="skill4">Next js</label>
             </div>
             <div className="btns">
-                <button id="Enroll">Enroll Student</button>
+                <button type='submit' id="Enroll">Enroll Student</button>
                 <button id="clear">Clear</button>
             </div>
         </form>
